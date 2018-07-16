815dd6e9c29c1d89e5486d93b2af05e67188f4c8
Brian Dill <bdill@leftandwrite.com>
2018-07-16 01:33:38 -0500

    add'l Jane Austin analysis

--- DataCamp/DC Course sentiment (Text Mining book).R
+++ DataCamp/DC Course sentiment (Text Mining book).R
@@ -24,6 +24,39 @@ library(stringr)
 library(tidyr)
 library(tidytext)
 
+# https://www.tidytextmining.com/sentiment.html#sentiment-analysis-with-inner-join
+tidy_books <- austen_books() %>%
+  group_by(book) %>%
+  mutate(linenumber = row_number(),
+         chapter = cumsum(str_detect(text, regex("^chapter [\\divxlc]", 
+                                                 ignore_case = TRUE)))) %>%
+  ungroup() %>%
+  unnest_tokens(word, text)
+
+nrc_joy <- get_sentiments("nrc") %>% 
+  filter(sentiment == "joy")
+
+tidy_books %>%
+  filter(book == "Emma") %>%
+  inner_join(nrc_joy) %>%
+  count(word, sort = TRUE)
+
+library(tidyr)
+
+# create index for every 80 lines, spread to + & - cols and get net sentiment
+jane_austen_sentiment <- tidy_books %>%
+  inner_join(get_sentiments("bing")) %>%
+  count(book, index = linenumber %/% 80, sentiment) %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
+
+library(ggplot2)
+
+#graph net sentiment over time (index) for each book
+ggplot(jane_austen_sentiment, aes(index, sentiment, fill = book)) +
+  geom_col(show.legend = FALSE) +
+  facet_wrap(~book, ncol = 2, scales = "free_x")
+
 
 
 #----- Project Gutenburg -------------------------------------------------------
@@ -64,10 +97,11 @@ data(stop_words)
 tidy_books <- tidy_books %>%
   anti_join(stop_words)
 
-tidy_books %>% 
-  inner_join(get_sentiments("bing"))
+
+
 #----- apply a lexicon sentiment -----
-sbooks <- tidy_books %>% inner_join(get_sentiments("bing"))
+sbooks <- tidy_books %>% 
+  inner_join(get_sentiments("bing"))
 
 swordcounts <- sbooks %>%
   group_by(word, sentiment) %>%
