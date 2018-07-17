923eb2c7249de7a44883b61990d745e0fbcba9e9
wbdill <bdill@leftandwrite.com>
2018-07-17 18:44:05 -0500

    .

--- DataCamp/DC Course sentiment.R
+++ DataCamp/DC Course sentiment.R
@@ -41,7 +41,7 @@ data(stop_words)
 tidy_books <- tidy_books %>%
   anti_join(stop_words)
 
-#-------------------------------------------------------------------------------
+#******************************************************************************
 
 tidy_shakespeare %>%
   inner_join(get_sentiments("bing")) %>%
@@ -52,6 +52,109 @@ tidy_shakespeare %>%
   mutate(sentiment = positive - negative)
 
 
-#-------------------------------------------------------------------------------
-# news
-#-------------------------------------------------------------------------------
+#******************************************************************************
+# ----- Pop songs -----
+
+# https://www.rdocumentation.org/packages/billboard/versions/0.1.0
+# download lyrics.rda from
+# https://github.com/mikkelkrogsholm/billboard/tree/master/data
+
+load("C:/Data/R/lyrics.rda")
+str(lyrics)
+
+tidy_lyrics <- lyrics %>%
+  unnest_tokens(word, lyrics)
+
+head(tidy_lyrics)
+
+# get total words per song (title) with count() and rename "n" to "total_words"
+totals <- tidy_lyrics %>%
+  count(title) %>%
+  rename(total_words = n)
+
+# stitch totals back with lyric words
+lyric_counts <- tidy_lyrics %>%
+  left_join(totals, by = "title")
+
+# join to "nrc" sentiment lexicon
+lyric_sentiment <- lyric_counts %>%
+  inner_join(get_sentiments("nrc"))
+
+head(lyric_sentiment)
+
+lyric_sentiment %>%
+  count(title, sentiment, sort = TRUE)
+
+# songs with most highest % of negative sentiment words
+lyric_sentiment %>%
+  count(title, sentiment, total_words) %>%
+  ungroup() %>%
+  mutate(percent = n / total_words) %>%
+  filter(sentiment == "negative") %>%
+  arrange(desc(percent))
+
+lyric_sentiment %>%
+  count(title, sentiment, total_words) %>%
+  ungroup() %>%
+  mutate(percent = n / total_words) %>%
+  filter(sentiment == "positive", total_words > 1) %>%
+  arrange(desc(percent))
+ 
+#----- plotting of pop songs ---------------------------------------------------
+# download lyrics.rda from
+# https://github.com/mikkelkrogsholm/billboard/tree/master/data
+load("C:/Data/R/wiki_hot_100s.rda")
+head(lyric_sentiment)
+head(wiki_hot_100s)
+
+# get song's top 100 rank from wiki_hot_100s and join to lyric_sentiment
+# rename some columns to match exercises from course
+lyric_sentiment2 <- lyric_sentiment %>% 
+  inner_join(wiki_hot_100s, by = "title") %>%
+  select(rank = no, song = title, artist = artist.x, year = year.x, word, total_words, sentiment)
+
+lyric_sentiment2$rank <- as.numeric(lyric_sentiment2$rank)
+lyric_sentiment2$year <- as.numeric(lyric_sentiment2$year)
+
+lyric_sentiment2 %>% filter(is.na(rank))
+
+
+# make boxplots to see if sentiment correlates to song's top rank
+lyric_sentiment2[complete.cases(lyric_sentiment2),] %>%
+  filter(sentiment == "positive", total_words > 5) %>%
+  count(song, rank, total_words) %>%
+  ungroup() %>%
+  mutate(percent = n / total_words,
+         rank = 10 * floor(rank / 10)) %>%
+  ggplot(aes(as.factor(rank), percent)) +
+    geom_boxplot()
+
+lyric_sentiment2[complete.cases(lyric_sentiment2),] %>%
+  filter(sentiment == "negative", total_words > 5) %>%
+  count(song, rank, total_words) %>%
+  ungroup() %>%
+  mutate(percent = n / total_words,
+         rank = 10 * floor(rank / 10)) %>%
+  ggplot(aes(as.factor(rank), percent)) +
+  geom_boxplot()  
+
+# no visible effect for either positive or negative sentiments...
+
+# ----- Sentiment scores by year -----
+lyric_sentiment2 %>%
+  filter(sentiment == "negative", total_words > 5) %>%
+  count(song, year, total_words) %>%
+  ungroup() %>%
+  mutate(percent = n / total_words,
+         year = 10 * floor(year / 10)) %>%
+  ggplot(aes(as.factor(year), percent)) + 
+  geom_boxplot()
+
+lyric_sentiment2 %>%
+  filter(sentiment == "positive", total_words > 5) %>%
+  count(song, year, total_words) %>%
+  ungroup() %>%
+  mutate(percent = n / total_words,
+         year = 10 * floor(year / 10)) %>%
+  ggplot(aes(as.factor(year), percent)) + 
+  geom_boxplot()
