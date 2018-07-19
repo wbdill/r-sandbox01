74c904450fb8b0d64a16cc919d334c9a8ec65265
wbdill <bdill@leftandwrite.com>
2018-07-19 11:03:49 -0500

    added comments

--- install.packages.R
+++ install.packages.R
@@ -3,13 +3,13 @@ install.packages("tidyverse")
 install.packages("stringi")
 
 # https://www.tidytextmining.com/
-install.packages("tidytext")    # text mining
-
-# https://cran.r-project.org/web/packages/mosaic/index.html
-install.packages("mosaic")      # stats pkg
+install.packages("tidytext")    # text mining (has built-in lexicon dictionaries)
 
 # https://cran.r-project.org/web/packages/syuzhet/index.html
 install.packages("syuzhet")     # sentiment lexicons
+
+# https://cran.r-project.org/web/packages/mosaic/index.html
+install.packages("mosaic")      # stats pkg
 
 # https://cran.r-project.org/web/packages/gutenbergr/index.html
 install.packages("gutenbergr")  # project Gutenberg book downloads
