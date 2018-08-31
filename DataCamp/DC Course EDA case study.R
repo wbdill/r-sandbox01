bfd5580ce6df1cc3c00c92a5bf1e8065ed8ee511
wbdill <bdill@leftandwrite.com>
2018-08-31 17:57:26 -0500

    chapter 4
    some scripts didn't work in R-studio, but DID work on the DataCamp web console.  I'm not sure why.

--- DataCamp/DC Course EDA case study.R
+++ DataCamp/DC Course EDA case study.R
@@ -3,11 +3,11 @@ rm(list = ls())
 library(dplyr)
 
 votes <- readRDS("C:/GitHub/r-sandbox01/DataCamp/data/votes.rds")
-desc <- readRDS("C:/GitHub/r-sandbox01/DataCamp/data/descriptions.rds")
+descriptions <- readRDS("C:/GitHub/r-sandbox01/DataCamp/data/descriptions.rds")
 
 str(votes)
 head(votes)
-str(desc)
+str(descriptions)
 
 install.packages("countrycode")
 library(countrycode)
@@ -83,3 +83,89 @@ UK_by_year <- by_year_country %>%
 UK_fit <- lm(percent_yes ~ year, UK_by_year)
 
 bind_rows(tidy(US_fit), tidy(UK_fit))
+
+#----- nest/unnest -----
+by_year_country <- by_year_country[complete.cases(by_year_country), ]
+
+library(purrr)
+library(tidyr)
+library(broom)
+
+# https://campus.datacamp.com/courses/exploratory-data-analysis-in-r-case-study/tidy-modeling-with-broom?ex=14
+# this works
+by_year_country %>%
+  nest(-country)
+
+#but this errors out
+by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, .)))
+# Error in mutate_impl(.data, dots) : Evaluation error: variable lengths differ (found for 'year').
+
+by_year_country %>%
+  nest(-country) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, .))) %>%
+  mutate(tidied = map(model, tidy)) %>%
+  unnest(tidied)
+
+#----- Chapter 4: Joining datasets -----
+glimpse(votes_processed)
+glimpse(descriptions)
+
+votes_joined <- votes_processed %>%
+  inner_join(descriptions, by = c("rcid", "session"))
+
+#me: Palestinian conflict
+#nu: Nuclear weapons and nuclear material
+#di: Arms control and disarmament
+#hr: Human rights
+#co: Colonialism
+#ec: Economic development
+
+# Filter, then summarize by year: US_co_by_year
+US_co_by_year <- votes_joined %>%
+  filter(co == 1, country == "United States") %>%
+  group_by(year) %>%
+  summarize(percent_yes = mean(vote == 1))
+
+ggplot(US_co_by_year, aes(year, percent_yes)) +
+  geom_line()
+
+votes_gathered <- votes_joined %>%
+  gather(topic, has_topic, me:ec) %>%
+  filter(has_topic == 1)
+
+#--- use recode() to make codes more readable.
+votes_tidied <- votes_gathered %>%
+  mutate(topic = recode(topic,
+                        me = "Palestinian conflict",
+                        nu = "Nuclear weapons and nuclear material",
+                        di = "Arms control and disarmament",
+                        hr = "Human rights",
+                        co = "Colonialism",
+                        ec = "Economic development"))
+ 
+by_country_year_topic <- votes_tidied %>%
+  group_by(country, year, topic) %>%
+  summarize(total = n(),
+            percent_yes = mean(vote == 1)) %>%
+  ungroup()
+
+by_country_year_topic %>%
+  filter(country == "United States") %>%
+  ggplot( aes(year, percent_yes)) + 
+  geom_line() +
+  facet_wrap(~ topic)
+
+# build models of country/topic combos  (this errors in R-studio for unknown reason so next block won't work either)
+country_topic_coefficients <- by_country_year_topic %>%
+  nest(-country, -topic) %>%
+  mutate(model = map(data, ~ lm(percent_yes ~ year, data = .)) ,
+         tidied = map(model, tidy)) %>%
+  unnest(tidied)
+
+# filter to just the slope (term == year) and adjusted p.value < .05
+country_topic_filtered <- country_topic_coefficients %>%
+  filter(term == "year") %>%
+  mutate(p.adjusted = p.adjust(p.value)) %>%
+  filter(p.adjusted < .05)
