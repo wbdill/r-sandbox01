025852b421ff0a6c63a9e7020da0c20907d401f1
wbdill <bdill@leftandwrite.com>
2018-12-03 17:21:12 -0600

    init

---
+++ DataCamp/DC Course Communicating with data in the Tidyverse.R
@@ -0,0 +1,11 @@
+# DC Course Communicating with data in the Tidyverse
+rm(list = ls())
+library(tidyverse)
+
+load("C:/GitHub/r-sandbox01/DataCamp/data/ilo_working_hours.RData")
+load("C:/GitHub/r-sandbox01/DataCamp/data/ilo_hourly_compensation.RData")
+
+glimpse(ilo_working_hours)
+glimpse(ilo_hourly_compensation)
+
+
