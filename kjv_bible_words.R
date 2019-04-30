41129c675f07d45622ba7375118ae88fcd15e0d0
wbdill <bdill@leftandwrite.com>
2019-04-30 12:57:14 -0500

    charts of NT gospels vs non-gospels

--- kjv_bible_words.R
+++ kjv_bible_words.R
@@ -69,25 +69,43 @@ png('C:/github/r-sandbox01/kjv_book_words.png', width = 1200, height = 900)
 plot(kjv_plot)
 dev.off()
 
-#----- Chart: Top 25 used words -----
+#----- Chart: Top 25 used words in KJV -----
+old_english_stop_words <- data.frame(word = c("thou", "thee", "thy", "ye", "hath", "hast", "thine"))
+
+kjv_tidy %>% 
+  anti_join(tidytext::stop_words) %>% 
+  anti_join(old_english_stop_words) %>%
+  filter(!is.na(word), str_detect(word, "[a-zA-Z]")) %>% 
+  #filter(book > 38 & book <= 42 ) %>%
+  #group_by(word) %>% 
+  count(word, sort = TRUE) %>%
+  #ungroup() %>% 
+  mutate(word = fct_rev(fct_inorder(word))) %>% 
+  slice(1:20) %>% 
+  ggplot() +
+  aes(word, n, label=n) +
+  geom_col() +
+  geom_text(hjust = -.2, size=4) +
+  coord_flip() +
+  labs(y = "Number of Times Mentioned", x = NULL, 
+       title = "20 Most Used Words in the KJV",
+       caption = "excludes common stop words such as: thou, thee, thy, a, and, the, etc.") 
+
+#----- Chart: Top 25 used words in KJV:NT -----
 kjv_tidy %>% 
   anti_join(tidytext::stop_words) %>% 
   filter(!is.na(word), str_detect(word, "[a-zA-Z]")) %>% 
-  #filter(book > 38 & book <= 42 ) %>%
-  group_by(word) %>% 
+  filter(is_nt == 1 ) %>%
   count(word, sort = TRUE) %>%
-  ungroup() %>% 
   mutate(word = fct_rev(fct_inorder(word))) %>% 
-  slice(1:30) %>% 
+  slice(1:20) %>% 
   ggplot() +
   aes(word, n) +
   geom_col() +
   coord_flip() +
   labs(y = "Number of Times Mentioned", x = NULL, 
-       title = "30 Most-Used Words in the KJV",
-       caption = "") 
-
-
+       title = "20 Most Used Words in the KJV New Testament",
+       caption = "excludes common stopwords") 
 
 
 #  scale_y_continuous(expand = c(0.01,0), limits = c(0, 2500)) +
@@ -100,3 +118,43 @@ kjv_tidy %>%
 #    panel.grid.major.y = element_blank(),
 #    plot.caption = element_text(hjust = 0)
 #  )
+
+
+#----- Chart: Top words of the gospels
+kjv_tidy %>% 
+  anti_join(tidytext::stop_words) %>% 
+  anti_join(old_english_stop_words) %>%
+  filter(!is.na(word), str_detect(word, "[a-zA-Z]")) %>% 
+  filter(book > 38 & book <= 43 ) %>%
+  count(word, sort = TRUE) %>%
+  slice(1:25) %>%
+  mutate(word = fct_rev(fct_inorder(word))) %>%
+  ggplot() +
+  aes(word, n, label = n) +
+  geom_col() +
+  coord_flip() +
+  geom_text(hjust = -.2, size=4) +
+  labs(y = "Number of Times Mentioned", x = NULL, 
+       title = "25 Most Used Words in the KJV Gospels",
+       subtitle = "Matthew, Mark, Luke, John",
+       caption = "excludes common stopwords") 
+
+#----- Chart: Top words of the NT EXCLUDING the gospels
+kjv_tidy %>% 
+  anti_join(tidytext::stop_words) %>% 
+  anti_join(old_english_stop_words) %>%
+  filter(!is.na(word), str_detect(word, "[a-zA-Z]")) %>% 
+  filter(book > 42  ) %>%
+  count(word, sort = TRUE) %>%
+  slice(1:25) %>%
+  mutate(word = fct_rev(fct_inorder(word))) %>%
+  ggplot() +
+  aes(word, n, label = n) +
+  geom_col() +
+  coord_flip() +
+  geom_text(hjust = -.2, size=3) +
+  labs(y = "Number of Times Mentioned", x = NULL, 
+       title = "25 Most Used Words in the KJV New Testament EXCLUDING the Gospels",
+       subtitle = "Books 43-66",
+       caption = "excludes common stopwords") 
+
