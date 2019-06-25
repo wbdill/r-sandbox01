539a10c2869af4994bc5275ceef76f36723152b5
wbdill <bdill@leftandwrite.com>
2019-06-25 18:05:53 -0500

    data.tables course

---
+++ DataCamp/DC Course data manipulation with data.table.R
@@ -0,0 +1,125 @@
+#---------------------
+# Coursera Data Manipulation in r with data.table (2019-06-24)
+# https://www.rdocumentation.org/packages/bikeshare14/versions/0.1.2
+rm(list = ls())
+library(data.table)
+#install.packages("bikeshare14")
+library(bikeshare14)
+
+#----- Chapter 1 -----
+# 3 ways to create data.table
+# data.table() as.data.table() fread()
+# data.table doesn't convert strings to factors and never sets or uses row names
+y <- list(id = 1:2, name = c("a", "b"))
+x <- as.data.table(y)
+
+# DT[i, j, by]   i = which rows, j=what to do, by = group by
+# .N = number of last row
+
+head(batrips)
+
+batrips <- as.data.table(batrips)
+batrips[bike_id == 473]
+
+indices(batrips)  # indexes are auto-created whena a column is filtered so future queries are faster
+
+batrips[1:10]
+batrips[start_station == "Ryland Park" & subscription_type != "Customer"]
+batrips[end_station %like% "Market$"]
+batrips[trip_id %in% c(588841, 139560, 139562)]
+batrips[duration %between% c(5000,6000)]
+batrips[start_station %chin% c("San Francisco City Hall", "Embarcadero at Sansome")]  # %chin% is %in% optimized for chars
+
+#----- Chapter 2 -----
+# .() is an alias for list()
+batrips[, c("bike_id", "trip_id")]
+batrips[, .(bike_id, trip_id)]
+
+batrips[, mean(duration)]
+batrips[start_station == "Japantown", .N] # how many rows started in Japantown?
+batrips[end_station == "Market at 10th", median(duration)]
+
+batrips[, difftime(end_date, start_date, units = "min")]  # ?difftime
+
+batrips[, .(mean_durn = mean(duration), last_ride = max(end_date))]
+
+
+#----- Chapter 3 -----
+batrips[ , .N, by = "start_station"]
+batrips[ , .N, by = .(start_station)]
+batrips[, .(mean_duration = mean(duration)), by = "start_station"]
+batrips[, .(mean_duration = mean(duration)), by = .(start_station, end_station)]
+
+batrips[, .(mean_duration = mean(duration)), by = .(start_station, month(start_date))]
+batrips[, .(mean_duration = mean(duration), total_trips = .N ), by = .(start_station, end_station)]
+batrips[, .(min_duration = min(duration), max_duration = max(duration) ), by = .(start_station, end_station, month(start_date))]
+
+# order by 
+batrips[, .N, by = .(start_station, 
+                     end_station)][order(-N)]
+
+batrips[, .N, by = end_station][order(-N)][1:5]  # top 5 destinations
+
+batrips[, .(start_date = start_date[c(1, .N)]), by = start_station]  # first and last ride for each start_station - this one seems weird.  I would pull them as 2 cols.
+
+# .SD() is "Subset of Data"
+batrips[, .SD[which.min(duration)], by = month(start_date), .SDcols = c("start_station", "end_station", "start_date", "end_date", "duration")]
+
+batrips[, lapply(.SD, uniqueN), by = month(start_date), .SDcols = c("start_station", "zip_code")]  # total number of unique start stations and zip codes per month
+
+#----- Chapter 4 -----
+# := creates/updates by reference
+ncol(batrips)
+batrips[, duration_mean := mean(duration), by = .(start_station, end_station)]  # creates new column
+ncol(batrips)
+
+batrips[duration_mean %between% c(1000, 1020), unique(duration_mean), by = .(start_station, end_station)][order(-V1)]
+
+batrips[, duration_mean, by = .(start_station, end_station)][order(duration_mean)]
+batrips[, duration_mean := NULL] #delete the column
+
+# in `:=`() functional form.  You have to wrap := in back-ticks since : is not a valid function name letter.
+batrips[, `:=`(is_dur_gt_1hour = duration > 3600, week_day = wday(start_date))]
+
+
+batrips[, trip_category := {
+    med_dur = median(duration, na.rm = TRUE)
+    if (med_dur < 600) "short"
+    else if (med_dur >= 600 & med_dur <= 1800) "medium"
+    else "long"
+  },
+  by = .(start_station, end_station)]
+batrips[1:3, ]
+
+# get min duration for each start/end station above 500
+batrips[duration > 500, min_dur_gt_500 := min(duration), by = .(start_station, end_station)]
+batrips[1:5]
+
+# two ways to add multiple columns.  1st is `:=` functional form.  2nd is <char_vector> := <list>
+ncol(batrips)
+batrips[, `:=`(mean_duration = NULL, median_duration = NULL)]
+batrips[, `:=`(mean_duration = as.integer(mean(duration)), median_duration = as.integer(median(duration))) , by = start_station]
+
+#2nd way to add multiple cols
+batrips[, c("mean_duration", "median_duration") := list(as.integer(mean(duration)), as.integer(median(duration))), by = start_station]
+
+
+#----- Chapter 5 -----
+# fread() is multi-threaded.  Dates and datetimes are read as character columns, but can be converted later (fasttime or anytime packages)
+# fread() can import from local file, URL, or string
+# skip = 9
+# nrows = 999 to limit number of rows imported
+# select = c("a", "b")  to limit which columns are imported.  If you need all but a few cols, use drop = c("a", "b") to get all but those cols.
+# na.strings = c("#N/A", "-", "")  specify what values should be translated to NA
+?fread
+d <- fread("a,b\n1,2\n3,4", verbose = TRUE)
+d
+
+comics <- fread("C:/GitHub/r-sandbox01/DataCamp/data/comics.csv")
+comics[appearances > 1000, .(name, id, align, appearances, first_appear, publisher)][order(-appearances)]
+
+
+?fwrite
+# dateTimeAs "ISO", "squash", "epoch".  Squash is ISO with hyphens/colons removed.  Epoch is days/seconds since 1970-01-01
+
+
--- DataCamp/DC Course writing functions.R
+++ DataCamp/DC Course writing functions.R
@@ -68,6 +68,15 @@ col_summary(cars, median)
 col_summary(cars, sd)
 col_summary(cars, IQR)
 
+#-----
+library(purrr)
+models <- mtcars %>% 
+  split(mtcars$cyl) %>%
+  map(~ lm(mpg ~ wt, data = .))
+
+coefs <- map(models, coef)
+map(coefs, "wt")
+
 
 
 #---------- Chapter 4 ----------
@@ -76,10 +85,69 @@ library(purrr)
 map_dbl(df, mean)
 map_dbl(df, median)
 map_dbl(df, sd)
+# safely() 
+# map2() iterate over 2 arguments.  map2( list(1,5,10), list(22, 33, 44), rnorm )
+# pmap() iterate over many arguments
+# invoke_map() iterate over functions   invoke_map(.f, .x)
 
-#---------- Chapter 5 ----------
+# walk(.f, .x) call to functions for their side effects rather than their return value.  Ex: to plot, or save to disk
+# walk2(.f, .x, .y) walk with 2 parameter lists
+# pwalk()
+
+#-----
+funs <- list(Normal = "rnorm", Uniform = "runif", Exp = "rexp")   # Define list of functions
+
+params <- list(                       # Define params
+  Normal = list(mean = 10),
+  Uniform = list(min = 0, max = 5),
+  Exp = list(rate = 5)
+)
+
+sims <- invoke_map(funs, params, n = 50)
+
+walk(sims, hist)           # Use walk() to make a histogram of each element in sims
 
 
+#-----
+# Define list of functions
+funs <- list(Normal = "rnorm", Uniform = "runif", Exp = "rexp")
 
+# Define params
+params <- list(
+  Normal = list(mean = 10),
+  Uniform = list(min = 0, max = 5),
+  Exp = list(rate = 5)
+)
 
+sims <- invoke_map(funs, params, n = 50)  # Assign the simulated samples to sims
+find_breaks <- function(x) {
+  rng <- range(x, na.rm = TRUE)
+  seq(rng[1], rng[2], length.out = 30)
+}
+nice_breaks <- map(sims, find_breaks)
+walk2(sims, nice_breaks, hist)
 
+#----- prettify the labels using pwalk()
+funs <- list(Normal = "rnorm", Uniform = "runif", Exp = "rexp")
+params <- list(
+  Normal = list(mean = 10),
+  Uniform = list(min = 0, max = 5),
+  Exp = list(rate = 5)
+)
+sims <- invoke_map(funs, params, n = 1000)
+nice_breaks <- map(sims, find_breaks)
+nice_titles <- c("Normal(10, 1)", "Uniform(0, 5)", "Exp(5)")
+pwalk(list(x = sims, breaks = nice_breaks, main = nice_titles), hist, xlab = "")
+
+#---------- Chapter 5: Robust functions ----------
+
+if(condition) {
+  stop("error message", call. = FALSE)
+}
+# avoid type-inconsistent functions in your own functions
+# avoid non-standard evaluation functions in your own functions
+# Never rely on global options for computational details
+
+options()
+getOption("digits")
+?options
