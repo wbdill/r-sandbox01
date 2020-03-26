22a53910cda00a977445c95797db44167e205123
Brian Dill <bdill@leftandwrite.com>
2020-03-26 16:37:25 -0500

    Update covidtracker.com_data.R

--- covid19/covidtracker.com_data.R
+++ covid19/covidtracker.com_data.R
@@ -17,18 +17,19 @@ statesdaily <- read_csv("http://covidtracking.com/api/states/daily.csv")
 
 statesdaily$date <- ymd(statesdaily$date)
 
+#----- Southeast states TESTS -----
 statesdaily %>%
-  filter(state %in% c("TN", "KY", "MS", "LA", "AL")) %>%
+  filter(state %in% c("TN", "KY", "MS", "LA", "AL", "GA")) %>%
   ggplot(aes(date, total, color = state)) +
   geom_line() + 
   labs(title = "Total Cumulative covid19 Tests",
        y = "Total Tests",
        caption = "source: http://covidtracking.com/api/states/daily.csv")
-ggsave(filename = "output/covidtracker.com_southeast_states_tests.png")
+ggsave(filename = "covidtracker.com_southeast_states_tests.png", path = "output")
 
-
+#----- Southeast states CASES -----
 statesdaily %>%
-  filter(state %in% c("TN", "KY", "MS", "LA", "AL")) %>%
+  filter(state %in% c("TN", "KY", "MS", "LA", "AL", "GA")) %>%
   ggplot(aes(date, positive, color = state)) +
   geom_line() + 
   labs(title = "Total Cumulative covid19 Confirmed Cases",
@@ -37,10 +38,19 @@ statesdaily %>%
 
 ggsave(filename = "output/covidtracker.com_southeast_states_cases.png")
 
-# factoring in population.
-statesdaily_withpop <- left_join(statesdaily, state_pop, by = c("state" = "state_abbrev"))
+#----- top states - TESTING -----
+statesdaily %>%
+  filter(state %in% c("NY", "WA", "CA", "MA", "FL")) %>%
+  ggplot(aes(date, total, color = state)) +
+  geom_line() +
+  labs(title = "covid19 Tests - Top 5 States",
+       y = "Total Cumulative Tests",
+       caption = "Source: http://covidtracking.com/api/states/daily.csv")
+ggsave(filename = "output/covidtracker.com_top5_testing_states.png")
 
-statesdaily_withpop %>%
+
+#----- top states - CASES -----
+statesdaily %>%
   filter(state %in% c("TN", "NY", "CA", "LA", "WA", "IL")) %>%
   ggplot(aes(date, positive, color = state)) +
   geom_line() + 
@@ -50,23 +60,31 @@ statesdaily_withpop %>%
 
 ggsave(filename = "output/covidtracker.com_southeast_states_cases_NY.png")
 
-# factoring in population.
+
+#----- factoring in population. -----
+statesdaily_withpop <- left_join(statesdaily, state_pop, by = c("state" = "state_abbrev"))
+
+#----- Big states cases per population. -----
 statesdaily_withpop %>%
   filter(state %in% c("TN", "NY", "CA", "LA", "WA", "IL")) %>%
   mutate(CasesPerM = positive / (population / 1000000) ) %>%
   ggplot(aes(date, CasesPerM, color = state)) +
   geom_line() + 
-  #scale_y_log10() +
   labs(title = "Total Cumulative covid19 Cases / Million Pop",
        y = "Total Tests",
        caption = "source: http://covidtracking.com/api/states/daily.csv")
 ggsave(filename = "output/covidtracker.com_southeast_states_cases_NY_per_mill.png")
 
 
+#statesdaily %>% filter(state == "TN")
 
 
-#----- states current -----
+#----- states current dataset -----
 states <- read_csv("https://covidtracking.com/api/states.csv")
 
 states %>% filter(state %in% c("TN", "KY", "MS", "LA", "AL"))
+
+
+
+
 
