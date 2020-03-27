4e72a712e7755079240bd4ed1637e90f3c419d73
Brian Dill <bdill@leftandwrite.com>
2020-03-26 21:08:15 -0500

    refinement of script captions.  Fixed states csv typos.

--- covid19/covidtracker.com_data.R
+++ covid19/covidtracker.com_data.R
@@ -8,83 +8,106 @@ library(lubridate)
 # https://www2.census.gov/programs-surveys/popest/tables/2010-2019/state/totals/nst-est2019-01.xlsx
 # Download directly here: https://pastebin.com/UAhAPiYB
 
-#state_pop <- read_csv("data/state_populations_2019.csv")
+#----- state populations -----
 state_pop <- read_csv("https://pastebin.com/raw/UAhAPiYB")
-#state_pop %>% arrange(desc(population))
+
+#----- states current dataset -----
+states_curr <- read_csv("https://covidtracking.com/api/states.csv")
 
 #----- states daily -----
-statesdaily <- read_csv("http://covidtracking.com/api/states/daily.csv")
+states_daily <- read_csv("http://covidtracking.com/api/states/daily.csv")
 
-statesdaily$date <- ymd(statesdaily$date)
+states_daily$date <- ymd(states_daily$date)
+
+#----- factoring in population. -----
+states_daily_withpop <- left_join(states_daily, state_pop, by = c("state" = "state_abbrev"))
+states_curr_withpop <- left_join(states_curr, state_pop,   by = c("state" = "state_abbrev"))
+
 
 #----- Southeast states TESTS -----
-statesdaily %>%
+states_daily %>%
   filter(state %in% c("TN", "KY", "MS", "LA", "AL", "GA")) %>%
-  ggplot(aes(date, total, color = state)) +
+  ggplot(aes(date, totalTestResults, color = state)) +
   geom_line() + 
   labs(title = "Total Cumulative covid19 Tests",
-       y = "Total Tests",
-       caption = "source: http://covidtracking.com/api/states/daily.csv")
+       subtitle = "Southeastern States",
+       y = "Cumulative Tests",
+       caption = "graph: @bdill   data: http://covidtracking.com/api/states/daily.csv")
 ggsave(filename = "covidtracker.com_southeast_states_tests.png", path = "output")
 
 #----- Southeast states CASES -----
-statesdaily %>%
+states_daily %>%
   filter(state %in% c("TN", "KY", "MS", "LA", "AL", "GA")) %>%
   ggplot(aes(date, positive, color = state)) +
   geom_line() + 
   labs(title = "Total Cumulative covid19 Confirmed Cases",
-       y = "Total Tests",
-       caption = "source: http://covidtracking.com/api/states/daily.csv")
+       subtitle = "Southeastern States",
+       y = "Cumulative Cases",
+       caption = "graph: @bdill   data: http://covidtracking.com/api/states/daily.csv")
 
 ggsave(filename = "output/covidtracker.com_southeast_states_cases.png")
 
 #----- top states - TESTING -----
-statesdaily %>%
-  filter(state %in% c("NY", "WA", "CA", "MA", "FL")) %>%
-  ggplot(aes(date, total, color = state)) +
+top5_states_tests <- states_curr %>%
+  arrange(desc(totalTestResults)) %>%
+  select(state, totalTestResults) %>%
+  top_n(5, totalTestResults)
+
+states_daily %>%
+  filter(state %in% pull(top5_states_tests, state)) %>%
+  ggplot(aes(date, totalTestResults, color = state)) +
   geom_line() +
-  labs(title = "covid19 Tests - Top 5 States",
-       y = "Total Cumulative Tests",
-       caption = "Source: http://covidtracking.com/api/states/daily.csv")
+  labs(title = "Cumulative covid19 Tests",
+       subtitle = "Top 5 States",
+       y = "Cumulative Tests",
+       caption = "graph: @bdill   data: http://covidtracking.com/api/states/daily.csv")
 ggsave(filename = "output/covidtracker.com_top5_testing_states.png")
 
 
 #----- top states - CASES -----
-statesdaily %>%
-  filter(state %in% c("TN", "NY", "CA", "LA", "WA", "IL")) %>%
+top5_states_cases <- states_curr %>%
+  arrange(desc(positive)) %>%
+  select(state, positive, positiveScore, datagrade = grade) %>%
+  top_n(5, positive)
+  
+
+pull(top5_states_cases, state) # pulls out states as a vector
+
+states_daily %>%
+  filter(state %in% pull(top5_states_cases, state)) %>%
   ggplot(aes(date, positive, color = state)) +
   geom_line() + 
-  labs(title = "Total Cumulative covid19 Cases",
-       y = "Total Tests",
-       caption = "source: http://covidtracking.com/api/states/daily.csv")
+  labs(title = "Cumulative Cases covid19",
+       subtitle = "Top 5 states",
+       y = "Cumulative Cases",
+       caption = "graph: @bdill   data: http://covidtracking.com/api/states/daily.csv")
 
-ggsave(filename = "output/covidtracker.com_southeast_states_cases_NY.png")
+ggsave(filename = "output/covidtracker.com_top5_states_cases_.png")
 
 
-#----- factoring in population. -----
-statesdaily_withpop <- left_join(statesdaily, state_pop, by = c("state" = "state_abbrev"))
 
 #----- Big states cases per population. -----
-statesdaily_withpop %>%
-  filter(state %in% c("TN", "NY", "CA", "LA", "WA", "IL")) %>%
+top5_state_cases_per_pop <- states_curr_withpop %>%
+  mutate(CasesPerM = positive / (population / 1000000)) %>%
+  select(state, positive, CasesPerM, population) %>%
+  top_n(5, CasesPerM) %>%
+  arrange(desc(CasesPerM))
+
+states_daily_withpop %>%
+  filter(state %in% pull(top5_state_cases_per_pop, state)) %>%
   mutate(CasesPerM = positive / (population / 1000000) ) %>%
   ggplot(aes(date, CasesPerM, color = state)) +
   geom_line() + 
-  labs(title = "Total Cumulative covid19 Cases / Million Pop",
-       y = "Total Tests",
-       caption = "source: http://covidtracking.com/api/states/daily.csv")
-ggsave(filename = "output/covidtracker.com_southeast_states_cases_NY_per_mill.png")
+  labs(title = "Cumulative covid19 Cases per Million Pop",
+       y = "Cumulative Cases per Million People",
+       caption = "graph: @bdill   data: http://covidtracking.com/api/states/daily.csv")
+ggsave(filename = "output/covidtracker.com_top5_states_cases_per_mill.png")
 
 
-#statesdaily %>% filter(state == "TN")
-
-
-#----- states current dataset -----
-states <- read_csv("https://covidtracking.com/api/states.csv")
-
-states %>% filter(state %in% c("TN", "KY", "MS", "LA", "AL"))
-
-
-
-
-
+states_curr_withpop %>%
+  select(state, region, population, cases = positive, tests = totalTestResults, deaths = death) %>%
+  mutate(CasesPerM = round(cases / (population / 1000000), digits = 1),
+         DeathsPerM = round(deaths / (population / 1000000), digits = 1),
+         TestsPerM = round(tests / (population / 1000000), digits = 1) ) %>%
+  arrange(desc(CasesPerM)) %>%
+  write_csv(path = "output/covid19_states_per_pop.csv")
