bd0176d30ba1037094552d2517434cae72b95771
Brian Dill <bdill@leftandwrite.com>
2020-10-07 18:49:02 -0500

    TN health dept

---
+++ DataCamp/DC Course String Manipulation with rtringr in R 2020-10-06.R
@@ -0,0 +1,26 @@
+# DC Course String Manipulation with rtringr in R
+# https://learn.datacamp.com/courses/string-manipulation-with-stringr-in-r
+# 2020-10-07
+rm(list = ls())
+
+catcidents <- readRDS("data/catcidents.rds")
+earnest <- read.delim("data/importance-of-being-earnest.txt")
+adverbs <- readRDS("data/adverbs.rds")
+narratives <- readRDS("data/narratives.rds")
+dna <- readRDS("data/dna.rds")
+
+#----- Ch 1 String Basics -----
+
+
+#----- Ch 2 Introduction to stringr -----
+
+
+#----- Ch 3 Pattern matching with regular expressions -----
+
+
+#----- Ch 4 More advanced matching and manipulation -----
+
+
+#----- Ch 5 Case studies -----
+
+
---
+++ covid19/TN_health_dept_covid.R
@@ -0,0 +1,56 @@
+# Williamson County TN
+
+library(readxl)
+library(tidyverse)
+
+d <- read_xlsx("data/TN-Public-Dataset-County-New.XLSX", col_types = c("date", "text", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric", "numeric"))
+?read_xlsx
+
+wmson <- d %>% 
+  filter(DATE >= "2020-04-01") %>% 
+  filter(COUNTY == "Williamson")
+  
+
+
+my_blue <- "#aaaaff"
+my_labs <- labs(title = "TITLE HERE",
+             subtitle = "Williamson County, TN ",
+             caption = "@bdill Source: https://www.tn.gov/health/cedep/ncov/data/downloadable-datasets.html (County New)")
+
+#----- New Cases -----
+wmson %>% 
+  filter(DATE >= "2020-05-01") %>% 
+  mutate(NEW_TESTS_07da = zoo::rollmean(NEW_TESTS, k = 7, fill = NA, align = "right")) %>% 
+  ggplot(aes(DATE, NEW_TESTS)) +
+  geom_col(fill = my_blue) +
+  geom_line(aes(DATE, NEW_TESTS_07da), color = "red", size = 1) +
+  my_labs
+
+#----- Positivity -----
+wmson %>% 
+  filter(DATE >= "2020-09-03") %>% 
+  mutate(test_positivity = NEW_POS_TESTS * 100 / NEW_TESTS,
+         test_positivity_07da = zoo::rollmean(test_positivity, k = 7, fill = NA, align = "right")) %>% 
+  ggplot(aes(DATE, test_positivity)) +
+  geom_col(fill = my_blue) +
+  geom_line(aes(DATE, test_positivity_07da), color = "red", size = 1) +
+  my_labs +
+  labs(title = "New Test Positivity")
+
+#----- TOTAL_HOSPITALIZED -----
+wmson %>% 
+  filter(DATE >= "2020-04-03") %>% 
+  pivot_longer(cols = c("TOTAL_HOSPITALIZED", "TOTAL_DEATHS"), names_to = "Stat") %>% 
+  select(DATE, Stat, value)  %>% 
+#select(DATE, TOTAL_HOSPITALIZED)
+  ggplot() +
+  geom_line(aes(DATE, value, color = Stat), size = 1) +
+#  geom_line(aes(DATE, TOTAL_DEATHS)) +
+  ylim(c(0, max(wmson$TOTAL_HOSPITALIZED))) +
+  #scale_y_continuous(limit = )
+  my_labs +
+  labs(title = "Total Deaths, Hospitalized")
+
+  ggsave("output/TN_Williamson_tot_death_hosp.png", width = 8, height = 5, dpi = 200)
+  
+  
