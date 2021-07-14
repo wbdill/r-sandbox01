2e7c3f6a1b20f08665eef126514a69f38d76d950
Brian Dill <bdill@leftandwrite.com>
2021-07-14 03:06:00 -0500

    Update 2020 Presidential votes vs vaccine rates 2021-07-13.R

--- 2020 Presidential votes vs vaccine rates 2021-07-13.R
+++ 2020 Presidential votes vs vaccine rates 2021-07-13.R
@@ -1,52 +1,59 @@
+#Desc:Chart of county level % of vote for Biden vs % vaccinated in 2021
+#Auth: Brian Dill (@bdill)
+#Date: 2021-07-14
 
 library(tidyverse)
+#rm(list = ls())
 
-rm(list = ls())
-
-#----- Presidential election results by county -----
+#----- Presidential election results by county (Harvard) -----
 # https://dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/VOQCHQ
 path <- "D:/opendata/PresidentialElection2000_2020/countypres_2000-2020.csv"
 pres <- read_csv(path)
 
-# pres %>% filter(year == 2020, county_fips == 40001)  # multiple modes
-
+# pres %>% filter(year == 2020, county_fips == 40001)  # multiple modes, so we need to summarize.
 
 pres2 <- pres %>% filter(year == 2020) %>% 
-  select(state_abb = state_po,
+  select(state = state_po,
          county = county_name,
          county_fips, 
          party, 
          candidate_votes = candidatevotes,
          tot_votes = totalvotes) %>%   
-  group_by(state_abb,county, county_fips, party, tot_votes) %>% 
-  summarize(candidate_votes = sum(candidate_votes)) %>% 
-  #filter(state_po == "OK") %>% 
+  group_by(state, county, county_fips, party, tot_votes) %>% 
+  summarize(candidate_votes = sum(candidate_votes)) %>%           # some states/counties have multiple rows (mail in, early, in person)
   mutate(candidate_pct = candidate_votes * 100 / tot_votes) %>% 
-  select(state_abb,
+  select(state,
          county,
-         county_fips, party, candidate_pct, tot_votes) %>% 
+         county_fips, 
+         party, 
+         candidate_pct, 
+         tot_votes) %>% 
   pivot_wider(names_from = party, values_from = candidate_pct, values_fill = 0) %>% 
-  arrange(state_abb, desc(REPUBLICAN))
+  arrange(state, desc(REPUBLICAN))
 
-pres2 %>% filter(state_abb == "TN")
+pres2 %>% filter(state == "TN")
 
-#----- vaccine percents CDC
+#----- vaccine percents by county (CDC) -----
 # https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-County/8xkx-amqh  (115MB csv)
 vax_path <- "D:/opendata/covid-19/vaccinations_by_county/COVID-19_Vaccinations_in_the_United_States_County.csv"
 vax <- read_csv(vax_path)
 #str(vax)
 
-vax$Date <- lubridate::mdy(vax$Date)
-vax$FIPS <- as.numeric(vax$FIPS)  # UNK coerced to na
+vax$Date <- lubridate::mdy(vax$Date)   # make date so we can filter to a specific date
+vax$FIPS <- as.numeric(vax$FIPS)       # make numeric so we can join to pres. UNK coerced to na
 
 vax2 <- vax %>% filter(Date == "2021-07-12") %>% 
-  select(date = Date, county_fips = FIPS, county = Recip_County, state = Recip_State, 
-         complete_pop_pct = Series_Complete_Pop_Pct, dose1_pop_pct = Administered_Dose1_Pop_Pct)
+  select(date = Date, 
+         county_fips = FIPS, 
+         county = Recip_County, 
+         state = Recip_State, 
+         complete_pop_pct = Series_Complete_Pop_Pct, 
+         dose1_pop_pct = Administered_Dose1_Pop_Pct)
 
 #----- votes vs vax -----
-votevax <- pres2 %>% inner_join(vax2, by = c("county_fips", "state_abb" = "state")) %>% 
-  select(state = state_abb, county = county.x, dem_pct = DEMOCRAT, rep_pct = REPUBLICAN, complete_pop_pct, dose1_pop_pct, tot_votes) %>% 
-  filter(complete_pop_pct > 0)
+votevax <- pres2 %>% inner_join(vax2, by = c("county_fips", "state")) %>% 
+  select(state, county = county.x, dem_pct = DEMOCRAT, rep_pct = REPUBLICAN, complete_pop_pct, dose1_pop_pct, tot_votes) %>% 
+  filter(complete_pop_pct > 0)  # filter out zero values b/c they are probably just missing.
 
 votevax %>%
   filter(state %in% c("CO", "TN", "MS", "CA", "FL", "GA")) %>% 
@@ -61,11 +68,9 @@ votevax %>%
        y = "pct of population completely vaccinated",
        caption = "Pres. Elec.: dataverse.harvard.edu/dataset.xhtml?persistentId=doi:10.7910/DVN/VOQCHQ\n Vax: data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-County/8xkx-amqh")
 
+#----- linear regression of % vaccinated by Dem vote % -----
 lm <- lm(complete_pop_pct ~ dem_pct, data = votevax)
 summary(lm)
 
-votevax %>% 
-  filter(state_abb == "TN", county == "WILLIAMSON") %>% 
-  arrange(desc(tot_votes)) 
 
 write_csv(votevax, "D:/opendata/vote_2020_vax_2021.csv")
