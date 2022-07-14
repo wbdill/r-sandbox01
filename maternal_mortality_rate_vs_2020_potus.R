fa3a1b9fd91e37769bc59390c3b95425ac92f1b4
Brian Dill <bdill@leftandwrite.com>
2022-07-14 09:47:39 -0500

    Update maternal_mortality_rate_vs_2020_potus.R

--- maternal_mortality_rate_vs_2020_potus.R
+++ maternal_mortality_rate_vs_2020_potus.R
@@ -1,19 +1,21 @@
 library(tidyverse)
 
 # https://worldpopulationreview.com/state-rankings/maternal-mortality-rate-by-state
-mort <- read_csv("D:/opendata/maternal_mortality_rate_2022.csv")  
-elec <- read_csv("D:/opendata/PresidentialElection1976_2020_state/2020_pres_state.csv")
+# https://pastebin.com/0TTaVW04
+mortality <- read_csv("https://pastebin.com/raw/0TTaVW04")  # "D:/opendata/maternal_mortality_rate_2022.csv"
 
-both <- elec %>% inner_join(mort, by = c("state" = "State"))
+#https://pastebin.com/NpMgqgiE
+election <- read_csv("https://pastebin.com/raw/NpMgqgiE")  # "D:/opendata/PresidentialElection1976_2020_state/2020_pres_state.csv"
 
-both %>% 
+join <- election %>% inner_join(mortality, by = c("state" = "State"))
+
+join %>% 
   ggplot(aes(x = pct_rep, y = maternalMortalityRate, label = state_abbrev)) +
   geom_point() +
   geom_smooth(method = lm) +
   geom_text(check_overlap = TRUE, nudge_x = .8, nudge_y = -.8) +
-  #scale(2) +
   labs(title = "Maternal Mortality Rate vs % Voted Republican in 2020",
        x = "% voted Republican in 2020",
        y = "Maternal Mortality Rate",
        caption = "chart: @bdill\ndata: https://worldpopulationreview.com/state-rankings/maternal-mortality-rate-by-state")
-?geom_text
+
