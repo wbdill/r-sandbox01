ba7f7e1c56fea8847cbccae0de7e8f3b74f1cd44
wbdill <bdill@leftandwrite.com>
2022-09-15 11:25:04 -0500

    Update KS_abortion_vote_2022.R

--- KS_abortion_vote_2022.R
+++ KS_abortion_vote_2022.R
@@ -27,3 +27,35 @@ join %>%
 #----- linear model -----
 mod <- lm(log(no) ~ pop, data = join)
 summary(mod)
+
+
+#--- No vote vs education attainment ---
+#----- Census educational attainment data ---
+edu <- get_acs(geography = "county",
+               state = "KS",
+               variables = c(bachelor_up_25_up = "S1501_C01_015",  # bachelor or higher age 25 or higher
+                             hs_up_25_up       = "S1501_C01_014",
+                             pop_25_up         = "S1501_C01_006"), # population age 25 or higher
+               survey = "acs5",
+               year = 2020,
+               output = "wide",
+               cache_table = TRUE)
+edu <- edu %>% 
+  mutate(pct_bach_up = bachelor_up_25_upE  * 100 / pop_25_upE,
+         county = str_remove(NAME, " County, Kansas"))
+
+vote_edu <- edu %>% 
+  inner_join(vote, by = "county")
+
+vote_edu %>% 
+  ggplot(aes(x = pct_bach_up, y = no)) +
+  geom_point() +
+  scale_x_log10(labels = comma) +
+  geom_smooth(method = lm) +
+  labs(title = "KS Abortion Vote by County",
+       x = "% of county (age 25 or up) with a bachelor's degree or higher",
+       y = "Pct voting No",
+       caption = "chart: @bdill\nEducation: US Census Bureau ACS 5-year 2020\nvote data: nytimes.com/interactive/2022/08/02/us/elections/results-kansas-abortion-amendment.html")
+
+mod <- lm(no ~ pct_bach_up, data = vote_edu)
+summary(mod)
